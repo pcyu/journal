8e3235372921e0f7489f63b40fc4bc4f9316c71d
Peter Yu <peteryu@gmail.com>
2020-04-15 17:03:49 +0800

    i can make my own fun shit

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> fuk this shit!
+          Edit <code>src/App.js</code> it became too much for me
         </p>
         <a
           className="App-link"
