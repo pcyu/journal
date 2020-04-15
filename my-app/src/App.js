aa77ed9ddb61c513ff0cb4bf878dac6c865cc9dc
Peter Yu <peteryu@gmail.com>
2020-04-15 16:50:57 +0800

    i give up

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> Let's sell it!
+          Edit <code>src/App.js</code> fuk this shit!
         </p>
         <a
           className="App-link"
