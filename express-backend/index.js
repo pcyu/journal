c1f677385b2f972c854bc4d76aa67631e343bef3
Peter Yu <peteryu@gmail.com>
2020-04-15 18:02:51 +0800

    set up index.js

--- express-backend/index.js
+++ express-backend/index.js
@@ -4,4 +4,14 @@ const cors = require("cors");
 const bodyParser = require("body-parser");
 const logger = require("morgan");
 const port = process.env.PORT || 3001;
+const playersRouter = require("./routes/players");
 
+app.use(logger('dev'));
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true}));
+app.use(bodyParser.json());
+app.use("/players", playersRouter);
+app.listen(port, function() {
+    console.log("Running on " + port);
+});
+module.exports = app;
