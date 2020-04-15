3a730f376c4eb8a9224ef61ecc3d28867884f1b2
Peter Yu <peteryu@gmail.com>
2020-04-15 18:49:36 +0800

    getting a backend rdy

---
+++ express-backend/dummyDatabase.js
@@ -0,0 +1,6 @@
+let players = [
+    { _id: 1, name: "Arlia", runs: '43211' },
+    { _id: 2, name: "Moo", runs: '21311' },
+    { _id: 3, name: "Shill", runs: '23511' }
+];
+module.exports = players;
--- express-backend/routes/players.js
+++ express-backend/routes/players.js
@@ -15,3 +15,21 @@ router.get("/list", async (req, res) => {
         });
     }
 });
+
+router.get("/list/:id", async (req, res) => {
+    let { id } = req.params;
+    id = Number(id);
+    try {
+        let player = players.find(player => player._id === id);
+        res.status(200).json({
+            data: player
+        });
+    } catch (err) {
+        res.status(400).json({
+            message: "Some error occured",
+            err
+        });
+    }
+});
+
+module.exports = router;
