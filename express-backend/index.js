e8c9bfedad7c9ee33383d266f706b84654a1f274
Peter Yu <peteryu@gmail.com>
2020-04-15 17:45:51 +0800

    i think i effed up

---
+++ express-backend/index.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const logger = require("morgan");
+const port = process.env.PORT || 3001;
+
